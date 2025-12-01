c0050b617bbfdec9462246c0319740cb215b0fec
Duc Nguyen <vduc@fastmail.com>
2025-12-01 11:37:31 +0100

    Add

--- multitrain.py
+++ multitrain.py
@@ -20,7 +20,7 @@ from tqdm.auto import tqdm
 import wandb
 from config import Config
 from ts_mamba.common import DummyWandb
-from ts_mamba.dataset import TileTimeSeriesDataset
+from ts_mamba.dataset import TileTimeSeriesDataset, TileTimeSeriesNonOverlappingDataset
 from ts_mamba.llm import MambaLMHeadModel 
 from ts_mamba.loss_eval import evaluate_model_rmse, evaluate_llm, evaluate_model_quantile
 from ts_mamba.model import TimeseriesModel, WeightedRMSELoss, MAELoss
@@ -117,12 +117,20 @@ def main(config: Config):
         train_df = pl.read_parquet(shard_path, memory_map=True)
 
         # Build dataset + loader
-        train_dataset = TileTimeSeriesDataset(
-            train_df,
-            train_meta,
-            context_length,
-            use_features=config.model.model != "llm"
-        )
+        if config.overlapping_train_data:
+            train_dataset = TileTimeSeriesDataset(
+                train_df,
+                train_meta,
+                context_length,
+                use_features=config.model.model != "llm"
+            )
+        else:
+            train_dataset = TileTimeSeriesNonOverlappingDataset(
+                train_df,
+                train_meta,
+                context_length,
+                use_features=config.model.model != "llm"
+            )
 
         # IMPORTANT: now that each rank has different data, do NOT use DistributedSampler
         train_sampler = None
